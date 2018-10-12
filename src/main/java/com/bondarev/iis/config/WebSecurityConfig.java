5df8b258a2bda6568273b3c8b4f68bbb61a309ba
byDirectX <gdirectx@gmail.com>
2018-10-12 23:43:24 +0300

    Add unit test and bcryptpasswordencoder

--- src/main/java/com/bondarev/iis/config/WebSecurityConfig.java
+++ src/main/java/com/bondarev/iis/config/WebSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.bondarev.iis.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -36,8 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("select name, password, active from user where name = ?")
                 .authoritiesByUsernameQuery("select u.name, ur.roles from user u inner join user_role ur on u.id = ur.user_id where u.name = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/bondarev/iis/controller/UserController.java
+++ src/main/java/com/bondarev/iis/controller/UserController.java
@@ -6,6 +6,7 @@ import com.bondarev.iis.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration() {
@@ -36,14 +38,15 @@ public class UserController {
             mav.addObject("message", "Пользователь с таким логином существует");
             mav.setViewName("registration");
 
-            log.info("Create user sucessfully");
-
             return mav;
         }
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(bcrypt.encode(user.getPassword()));
         userService.addUser(user);
+
+        log.info("Create user sucessfully");
 
         mav.setViewName("redirect:/login");
         return mav;
